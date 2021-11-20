3381ff78f036c37401113411da1e3c2e739a96a7
Krishna_chaithanya <krishna@immigrate.biz>
2021-11-20 18:43:09 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <GroupA />
-  <GroupB />
-  <GroupC />
+  <v-container>
+    <v-card>
+      <v-tabs v-model="tab" background-color="primary" dark>
+        <v-tab v-for="item in items" :key="item.tab">
+          {{ item.tab }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="item in items" :key="item.tab">
+          <v-card flat>
+            <v-card-text>
+              <component v-bind:is="item.content"></component>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -16,6 +31,18 @@ export default {
     GroupB,
     GroupC,
   },
+  data() {
+    return {
+      tab: null,
+      items: [
+        { tab: "Group_A", content: "GroupA" },
+        { tab: "Group_B", content: "GroupB" },
+        { tab: "Group_C", content: "GroupC" },
+      ],
+    };
+  },
+
+  methods: {},
 };
 </script>
 
